1aadf58c21f74517c704674470ef92e0758040ad
TanyaKomyakova <tanya_komyakova@mail.ru>
2020-08-25 14:31:19 +0300

    test falls

--- src/test/java/ru/netology/DeliveryTest.java
+++ src/test/java/ru/netology/DeliveryTest.java
@@ -3,6 +3,11 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -11,18 +16,28 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryTest {
+
+
     @Test
     void checkingCardBooking(){
         open("http://localhost:9999");
         SelenideElement form = $("form");
         form.$("[data-test-id=city] input").setValue("Москва");
-        form.$(".calendar-input input").click();
+        form.$(".calendar-input input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$(".calendar-input input").setValue("dateMetting");
+        String dateMetting = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         form.$("[data-test-id=name] input").setValue("Комякова Татьяна");
         form.$("[data-test-id=phone] input").setValue("+79631645544");
         form.$("[data-test-id=agreement]").click();
-        $(withText("Забронировать")).waitUntil(visible,15000);
-        form.$("button").click();
-        $("[data-test-id=notification]").exists();
+        form.$(".button__text").click();
+        $(withText("Успешно!")).waitUntil(visible,15000);
+
+
+
+
+
+
+
 
 
 
